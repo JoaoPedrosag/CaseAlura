b017c2901210182bc203d579b09a43d00b907413
Joao pedro Goettems Saggiorato <89108658+JoaoPedrosag@users.noreply.github.com>
2023-01-18 16:36:46 -0300

    remove key

--- lib/core/const/api_utl.dart
+++ lib/core/const/api_utl.dart
@@ -1,5 +1,5 @@
 class Links {
-  static const key = '983928bb76ee533d2e6b4d52986438eb';
+  static const key = '';
   static const base = "https://api.themoviedb.org/3/movie/";
   static const language = "language=pt-BR";
 }
